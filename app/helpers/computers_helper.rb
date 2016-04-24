10fae83502e2004ae5bd49e59c6056608ee81618
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2016-04-24 17:11:20 -0400

    Did some refactoring in the computer view

--- app/helpers/computers_helper.rb
+++ app/helpers/computers_helper.rb
@@ -1,2 +1,9 @@
 module ComputersHelper
+  def conditional_link(model,method,message)
+    if model.send(method) == ""
+      link_to message, controller: "#{model.model_name.to_s.downcase}s", action: "show", id: model
+    else
+      link_to model.name, controller: "#{model.model_name.to_s.downcase}s", action: "show", id: model
+    end
+  end
 end
